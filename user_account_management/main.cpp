1755b31b148798127c6a07677f8270ef2b79635a
inkeyes <inkeyes@tutanota.com>
2022-06-06 19:01:18 -0400

    Added initial template code for main menu loop.

--- user_account_management/main.cpp
+++ user_account_management/main.cpp
@@ -3,13 +3,35 @@
 int main()
 {
 	Account newaccount;
+	int menuSelect = 9;
 
-	newaccount.setName();
+	while (menuSelect != 0) {
+		std::cout << "Welcome to the menu!"
+			<< "Please select from the following options:"
+			<< std::endl
+			<< "1. Login with an existing account"
+			<< "2. Create a new account"
+			<< "0. Exit the program";
+
+
+		switch (menuSelect) {
+			case 1:
+				std::cout << "Login code here";
+			case 2:
+				std::cout << "Account creation code here";
+			case 0:
+				std::cout << "Exit code here";
+			default:
+				
+		}
+	}
+
+	/*newaccount.setName();
 	newaccount.setPass();
 	std::cout << std::endl;
 	newaccount.printName();
 	newaccount.printPass();
 
 	std::cin.ignore();
-	std::cin.get();
+	std::cin.get();*/
 }
