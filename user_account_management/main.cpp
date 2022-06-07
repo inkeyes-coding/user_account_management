147d0f605d3851c2a8392c6e9aa0183d3cc442f7
inkeyes <inkeyes@tutanota.com>
2022-06-07 12:50:09 -0400

    Further structure added to the main menu.

--- user_account_management/main.cpp
+++ user_account_management/main.cpp
@@ -5,24 +5,31 @@ int main()
 	Account newaccount;
 	int menuSelect = 9;
 
-	while (menuSelect != 0) {
-		std::cout << "Welcome to the menu!"
-			<< "Please select from the following options:"
+	while (menuSelect != 0)
+	{
+		std::cout << "Welcome to the menu!" << std::endl
+			<< "Please select from the following options:" << std::endl
 			<< std::endl
-			<< "1. Login with an existing account"
-			<< "2. Create a new account"
-			<< "0. Exit the program";
+			<< "1. Login with an existing account" << std::endl
+			<< "2. Create a new account" << std::endl
+			<< "0. Exit the program" << std::endl << std::endl;
 
+		std::cin >> menuSelect;
 
-		switch (menuSelect) {
+		switch (menuSelect)
+		{
 			case 1:
-				std::cout << "Login code here";
+				std::cout << "Login code here" << std::endl;
+				break;
 			case 2:
-				std::cout << "Account creation code here";
+				std::cout << "Account creation code here" << std::endl;
+				break;
 			case 0:
-				std::cout << "Exit code here";
+				std::cout << "Exit code here" << std::endl;
+				break;
 			default:
-				
+				std::cout << "Invalid option selected. Please try again." << std::endl;
+				break;
 		}
 	}
 
@@ -30,8 +37,8 @@ int main()
 	newaccount.setPass();
 	std::cout << std::endl;
 	newaccount.printName();
-	newaccount.printPass();
+	newaccount.printPass();*/
 
-	std::cin.ignore();
+	/*std::cin.ignore();
 	std::cin.get();*/
 }
